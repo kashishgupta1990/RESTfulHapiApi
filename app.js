bb2b10188c19bafb788b94ed1389f3b6d0ead152
kashish <kashishgupta1990@yahoo.com>
2015-05-24 18:19:59 +0530

    added 1_BaseSetup.js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+// Include Hapi package
 var Hapi = require('hapi');
+
+// Create Server Object
 var server = new Hapi.Server();
+
+// Include Mongoose ORM to connect with database
 var mongoose = require('mongoose');
+
+// Include Joi package to validate request params and payload.
 var Joi = require('joi');
+
+// Making connection with `restdemo` database in your local machine
 mongoose.connect('mongodb://localhost/restdemo');
+
+// Importing `user` model from `models/user.js` file
 var UserModel = require('./models/user');
 
+// Define PORT number You can change it if you want
 server.connection({port: 7002});
 
+// Register Swagger Plugin ( Use for documentation and testing purpose )
 server.register({
     register: require('hapi-swagger'),
     options: {
@@ -20,6 +33,7 @@ server.register({
     }
 });
 
+// Register Good Plugin ( Use to log API url's hit on serer )
 server.register({
     register: require('good'),
     options: {
@@ -37,25 +51,28 @@ server.register({
 
 });
 
+// Fetching all users data
 server.route({
     method: 'GET',
     path: '/api/user',
     config: {
+        // Include this API in swagger documentation
         tags: ['api'],
         description: 'Get All User data',
         notes: 'Get All User data'
     },
     handler: function (request, reply) {
+        //Fetch all data from mongodb User Collection
         UserModel.find({}, function (error, data) {
             if (error) {
                 reply({
-                    status: 503,
+                    statusCode: 503,
                     message: 'Failed to get data',
                     data: error
                 });
             } else {
                 reply({
-                    status: 200,
+                    statusCode: 200,
                     message: 'User Data Successfully Fetched',
                     data: data
                 });
@@ -66,6 +83,7 @@ server.route({
 
 server.route({
     method: 'GET',
+    //Getting data for particular user "/api/user/1212313123"
     path: '/api/user/{id}',
     config: {
         tags: ['api'],
@@ -78,16 +96,18 @@ server.route({
         }
     },
     handler: function (request, reply) {
+
+        //Finding user for particular userID
         UserModel.find({_id: request.params.id}, function (error, data) {
             if (error) {
                 reply({
-                    status: 503,
+                    statusCode: 503,
                     message: 'Failed to get data',
                     data: error
                 });
             } else {
                 reply({
-                    status: 200,
+                    statusCode: 200,
                     message: 'User Data Successfully Fetched',
                     data: data
                 });
@@ -100,11 +120,15 @@ server.route({
     method: 'PUT',
     path: '/api/user/{id}',
     config: {
+        // Swagger documentation fields tags, description, note
         tags: ['api'],
         description: 'Update specific user data',
         notes: 'Update specific user data',
+
+        // Joi api validation
         validate: {
             params: {
+                //`id` is required field and can only accept string data
                 id: Joi.string().required()
             },
             payload: {
@@ -114,16 +138,18 @@ server.route({
         }
     },
     handler: function (request, reply) {
+
+        // `findOneAndUpdate` is a mongoose modal methods to update a particular record.
         UserModel.findOneAndUpdate({_id: request.params.id}, request.payload, function (error, data) {
             if (error) {
                 reply({
-                    status: 503,
+                    statusCode: 503,
                     message: 'Failed to get data',
                     data: error
                 });
             } else {
                 reply({
-                    status: 200,
+                    statusCode: 200,
                     message: 'User Updated Successfully',
                     data: data
                 });
@@ -149,7 +175,10 @@ server.route({
     },
     handler: function (request, reply) {
 
+        // Create mongodb user object to save it into database
         var user = new UserModel(request.payload);
+
+        //Call save methods to save data into database and pass callback methods to handle error
         user.save(function (error) {
             if (error) {
                 reply({
@@ -181,16 +210,17 @@ server.route({
     },
     handler: function (request, reply) {
 
+        // `findOneAndRemove` is a mongoose methods to remove a particular record into database.
         UserModel.findOneAndRemove({_id: request.params.id}, function (error) {
             if (error) {
                 reply({
-                    status: 503,
+                    statusCode: 503,
                     message: 'Error in removing User',
                     data: error
                 });
             } else {
                 reply({
-                    status: 200,
+                    statusCode: 200,
                     message: 'User Deleted Successfully'
                 });
             }
@@ -199,7 +229,7 @@ server.route({
     }
 });
 
-
+// Lets start the server
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
