714867732a95f927cb1c9056764838d8ef90ca53
kashish <kashishgupta1990@yahoo.com>
2015-05-24 18:20:17 +0530

    added 1_BaseSetup.js

---
+++ test/1_BaseSetup.js
@@ -0,0 +1,40 @@
+// Include Hapi package
+var Hapi = require('hapi');
+
+// Create Server Object
+var server = new Hapi.Server();
+
+// Define PORT number
+server.connection({port: 7002});
+
+// Define GET route
+server.route({
+    method: 'GET',      // Methods Type
+    path: '/api/user',  // Url
+    handler: function (request, reply) { //Action
+
+        // Response JSON object
+        reply({
+            statusCode: 200,
+            message: 'Getting All User Data',
+            data: [
+                {
+                    name:'Kashish',
+                    age:24
+                },
+                {
+                    name:'Shubham',
+                    age:21
+                },
+                {
+                    name:'Jasmine',
+                    age:24
+                }
+            ]
+        });
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
